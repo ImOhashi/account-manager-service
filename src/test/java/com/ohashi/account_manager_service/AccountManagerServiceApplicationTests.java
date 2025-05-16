b39657a0c2defe59802e5cb25db6ba7f774e15c6
ImOhashi <leonardoohashi.mkt@gmail.com>
2025-05-15 21:55:36 -0300

    first commit

---
+++ src/test/java/com/ohashi/account_manager_service/AccountManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.account_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
